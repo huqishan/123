af25884c13eca02eb841ffa0ecfc56636611b792
Administrator <Administrator@WIN-GA31V8FQD7F>
2026-04-21 18:59:19 +0800

    1.测试3

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        //测试2
+        //测试23
         private NavigationViewItem? _TestMenuItem;
         private NavigationViewItem? _HomeMenuItem;
         List<ControlInfoDataItem> _NavigationInfo = new List<ControlInfoDataItem>();
